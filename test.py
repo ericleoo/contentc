48a7e63f10bde10952b42a4c6c0112bd32ff29a9
I337902 <eric.lim01@sap.com>
2017-03-16 10:53:28 +0800

    C

--- test.py
+++ test.py
@@ -3,6 +3,47 @@ import json
 from keras.preprocessing import sequence
 from keras.models import model_from_json
 
+def clean(s,z=""):
+    "clean sentences"
+    words = s.split(' ')
+    PUNC = '‐‑‒–—―_,.:?!"()*-;/\\][{}]&­' + "'"
+    URL = ["http",'ftp','://','.com','.net','.edu','.org','.uk','.gov']
+    #stemmer = PorterStemmer()
+    
+    for i in range(len(words)): 
+        words[i] = words[i].strip(PUNC)
+        #words[i] = stemmer.stem(words[i])
+        if words[i].find('@') != -1:
+            for it in URL[3:]:
+                if words[i].find(it) != -1:
+                    words[i] = '<email>'
+        
+        for it in URL:
+            if words[i].find(it) != -1:
+                words[i] = '<url>'
+    ret = re.sub('[' + re.escape(PUNC) + ']',' ',' '.join(words)).strip()   
+    ret = re.sub('[^'+chr(0)+'-'+chr(177)+']','',ret).strip()
+    ret = ret.lower()
+    ret = re.sub(r'[\n\r]( )*',' ',ret).strip()
+    ret = re.sub(r'( )*[\n\r]',' ',ret).strip()
+    ret = re.sub(r'[/&]',' ',ret).strip()
+    ret = re.sub(r', ',' ',ret).strip()
+    ret = re.sub(r',','',ret).strip()
+    ret = re.sub(r'( )+',' ',ret).strip()
+    ret = re.sub(r'^(18|19|20)[0-9][0-9][^0-9]','<y> ',ret)
+    ret = re.sub(r'^(18|19|20)[0-9][0-9]$','<y>',ret)
+    ret = re.sub(r'[^0-9](18|19|20)[0-9][0-9][^0-9]',' <y> ',ret)
+    ret = re.sub(r'[^0-9](18|19|20)[0-9][0-9]$',' <y>',ret)
+    months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+    for it in months: ret = re.sub(it,'<m>',ret)
+    ret = re.sub('<m> to <m>','<m> - <m>',ret)
+    ret = re.sub('<y> to <m>','<y> - <m>',ret)
+    ret = re.sub('<m> to <y>','<m> - <y>',ret)
+    ret = re.sub('<y> to <y>','<y> - <y>',ret)
+    if z == 'skills':
+        ret = re.sub('[0-9]+(\+)? year(s?)', '', ret)
+    return ret.strip()
+
 def getLabel(x):
     labels = {'edu': 1, 'job': 0, 'skills': 2, 'awards': 3, 'link':4, 'certification': 5, 'national_service': 6,'patent': 7, 'group': 8, 'additional': 9}
     labels = {it[1]:it[0] for it in labels.items()}   
@@ -31,7 +72,7 @@ for i in range(len(sentences)): sentences[i] = sentences[i].strip()
 
 print("Reading training")
 for instance in sentences:
-	sentence = instance[:]
+	sentence = clean(instance[:])
 	sentence = [it for it in re.split('[^a-z<>]',sentence) if it]
 	for i,word in enumerate(sentence):
 		if word.strip() in word2idx:
